bcedae7f133937416f510c329fccbf46940ac068
Sharpryan20 <sharpryan01@icloud.com>
2021-12-02 14:50:03 +0000

    Created PlayGame() function and created title image

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ UserStats = {
     "weapon": None
 }
 
-def Story_text(text, delay):
+def Story_Text(text, delay):
     """
     Prints out a line of text for the story and delays
     the next line for a certain amount of time.
@@ -23,5 +23,23 @@ def Stats(healthNumber):
 
     UserStats["health"] = UserStats["health"] + (healthNumber)
 
-
+def PlayGame():
+    """
+    Function called at the end of the run.py file. 
+    The only function called on a global scale.
+    """
+    print('''\033[94m
+    _______ ___   ___ ______
+   |__   __|   | |   |    __|
+      | |  |   |_|   |   |__
+      | |  |    _    |    __|
+      | |  |   | |   |   |__
+      |_|  |___| |___|______|.\033[96m
+   ___   ___ ___   ___ ____     __ _______
+  |   | |   |   | |   |    \   |  |__   __|
+  |   |_|   |   | |   |     \  |  |  | |
+  |    _    |   |_|   |      \ |  |  | |
+  |   | |   |         |   |\  \|  |  | |
+  |___| |___|_________|___| \_____|  |_|
+    \033[0m\n''')
 
